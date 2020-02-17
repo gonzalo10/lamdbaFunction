01568994a7a0df29e131edf4a861c34c0df9ca5e
angel <nopaixx@gmail.com>
2020-02-17 23:56:24 +0100

    deploy with lambda

---
+++ project/__init__.py
@@ -0,0 +1,13 @@
+from newspaper import Article
+import json
+
+
+def lambda_function_01(event, context):
+    url = 'http://fox13now.com/2013/12/30/new-year-new-laws-obamacare-pot-guns-and-drones/'
+    article = Article(url)
+    article.download()
+    article.text
+    return {
+        'statusCode': 200,
+        'body': json.dumps(article.text)
+    }
