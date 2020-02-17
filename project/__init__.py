52e8c9dbb2ba60004ab47d3f11a7640b20883a20
angel <nopaixx@gmail.com>
2020-02-18 00:37:44 +0100

    done

--- project/__init__.py
+++ project/__init__.py
@@ -6,7 +6,7 @@ def lambda_function_01(event, context):
     url = 'http://fox13now.com/2013/12/30/new-year-new-laws-obamacare-pot-guns-and-drones/'
     article = Article(url)
     article.download()
-    article.text
+    article.parse()
     return {
         'statusCode': 200,
         'body': json.dumps(article.text)
